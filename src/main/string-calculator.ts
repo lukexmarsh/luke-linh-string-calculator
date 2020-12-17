d367be526475d6aa40d51d5829ffa18ca5a2da13
Luke Marsh <luke.marsh@cazoo.co.uk>
2020-12-17 10:24:28 +0000

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/main/string-calculator.ts
+++ src/main/string-calculator.ts
@@ -1,3 +1,4 @@
 export function Add(numbers: string) {
   if (numbers === "") return 0;
+  return Number(numbers[0]) + Number(numbers[2]);
 }
--- src/test/string-calculator.test.ts
+++ src/test/string-calculator.test.ts
@@ -4,4 +4,7 @@ describe("string calculator", () => {
   it("returns 0 when receiving an empty string", () => {
     expect(Add("")).toEqual(0);
   });
+  it("returns the sum of 2 numbers received", () => {
+    expect(Add("3,5")).toEqual(8);
+  });
 });
