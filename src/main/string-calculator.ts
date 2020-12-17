9cba610f3d9b63ebb054639b5cbc8feb7e07b62e
linhcazoo <linh.nguyen@cazoo.co.uk>
2020-12-17 10:34:39 +0000

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/main/string-calculator.ts
+++ src/main/string-calculator.ts
@@ -1,4 +1,9 @@
 export function Add(numbers: string) {
   if (numbers === "") return 0;
-  return Number(numbers[0]) + Number(numbers[2]);
+  if (numbers.length === 3) {
+    return Number(numbers[0]) + Number(numbers[2]);
+  }
+  if (numbers.length > 3) {
+    return Number(numbers[0]) + Number(numbers[2]) + Number(numbers[4]) + Number(numbers[6]);
+  }
 }
